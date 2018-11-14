a1a6fdc42e3c9e59dcfed1054e0c17386be026b9
fuchenxu2008 <fuchenxu2008@163.com>
2018-11-14 13:58:18 +0800

    crawler modification

--- server/controllers/WebCrawler/getRecipes.js
+++ server/controllers/WebCrawler/getRecipes.js
@@ -1,52 +1,41 @@
 const axios = require("axios")
-const {addReceipe} = require("../recipeController")
+const { addReceipe } = require("../recipeController")
 
-async function getCategoryID() {
-    const {
-        data
-    } = await axios.get("http://apis.juhe.cn/cook/category?key=a0c106ec027b8ac08cd151cdb0a203a2&parentid=10002")
-    return data.result[0].list;
-}
+const API_URL = 'http://apis.juhe.cn/cook'
+const API_KEY = 'a0c106ec027b8ac08cd151cdb0a203a2'
 
-async function sleep(ms){
+async function sleep(ms) {
     return new Promise(resolve => setTimeout(() => resolve(), ms))
 }
 
-async function getRecipe(id,cata) {
+async function getCategoryIdList() {
+    const { data } = await axios.get(`${API_URL}/category?key=${API_KEY}&parentid=10002`)
+    return data.result[0].list.map(cata => ({ id: cata.id, name: cata.name }))
+}
+
+async function getRecipe(id, cata, rn = 30) {
     let finish = false
     let index = 0
     while (!finish) {
-        const {data} = await axios.get(`http://apis.juhe.cn/cook/index?key=a0c106ec027b8ac08cd151cdb0a203a2&cid=${id}&pn=${index}&rn=30`)
+        const { data } = await axios.get(`${API_URL}/index?key=${API_KEY}&cid=${id}&pn=${index}&rn=${rn}`)
         if (!data.result) {
             console.log('Catagory of', cata, 'is done!')
             finish = true;
         } else {
-            // console.log("index:", index)
-            index += 30     
-            // decide difficulty level
-            // implement data in the mongodb database
+            index += rn     
             for (const recipe of data.result.data) {
-                //extract the tags
-                recipe.tags = recipe.tags.split(";")[0]
-                addReceipe(recipe,cata)
+                addReceipe(recipe, cata)
                 await sleep(100)
             }
         }
     }
 }
 
-
 async function main() { 
-    // const categoryList = await getCategoryID();
-    // const categoryIdList = categoryList.map(categoryObj => categoryObj.id)
-    // console.log("categoryIdList:", categoryIdList)
-    const { data } = await axios.get("http://apis.juhe.cn/cook/category?parentid=10002&dtype=&key=a0c106ec027b8ac08cd151cdb0a203a2")
-    const list = data.result[0].list.map(cata => ({ id: cata.id, name: cata.name }))
-    // console.log(list)
-    for (const cata of list) {
-        console.log("categoryId:", cata.id, 'name:', cata.name)
-        await getRecipe(cata.id,cata.name)
-        await sleep(500)
+    const categoryList = await getCategoryIdList();
+    for (const cata of categoryList) {
+        await getRecipe(cata.id, cata.name)
+        await sleep(100)
     } 
 }
 
