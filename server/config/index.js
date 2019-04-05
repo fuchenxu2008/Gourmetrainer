cf2665e64f05e95f120bde718ec2149f7905b024
fuchenxu2008 <fuchenxu2008@163.com>
2019-04-05 17:40:41 +0800

    db auth

--- server/config/index.js
+++ server/config/index.js
@@ -2,5 +2,5 @@ const { uname, psw, host, port, database } = require('./db.json');
 
 module.exports = {
     port: process.env.PORT || 3333,
-    mongoUrl: `mongodb://${uname}:${psw}@${host}:${port}/${database}`
+    mongoUrl: `mongodb://${uname}:${psw}@${host}:${port}/${database}?authSource=admin`
 }
